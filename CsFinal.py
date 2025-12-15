b8174f0609a448f661a72521f0e765a18cb235f6
Jah-Jah-knows <jonahctchang@gmail.com>
2025-12-15 22:39:14 +0000

    import project files

---
+++ CsFinal.py
@@ -0,0 +1,165 @@
+###Cs Final Project Jonah Chang and Wyatt Miyamoto BatterUp! Whitman Baseball
+'''
+Used streamlit.io for information on formatting and syntax within the library
+Consulted chatGPT for basic examples to build off of as well as for debugging help
+Would also like to credit our teammates and Coach Jordan Hara for their videos and 
+input on earlier versions of the game. 
+'''
+#Import libraries and dictionaries
+import random
+import streamlit as st
+import PlayerInfo
+import string
+import pandas as pd
+
+#define remove punctuation function
+def removePunctuation(text):
+	'''
+	Removes punctuation from a given piece of text
+	pre-conditions: piece of text
+	post-conditions: none
+	'''
+	newText = ""
+	for character in text:
+		if character not in string.punctuation:
+			newText += character
+	return newText
+
+# define function for play again
+def resetGame():
+	'''
+	Resets game to original conditions and generates a new secret name
+	pre-conditions: none
+	post-conditions: game state that has no previous user interaction 
+	and has a new name to guess 
+	'''
+	st.session_state.game_over = False
+	st.session_state.guessInput = "" 
+	st.session_state.secretName = st.session_state.remaining.pop()
+	st.session_state.tries = 0
+	st.session_state.guessHistory = []
+
+#define make rows function
+def makeRows(guess, target):
+	'''
+	creates rows of values to be used in the grid that displays previous guesses
+	pre-conditions: a list of keys that correspond to values in the dictionary,
+	a guess inputted by the player and a target player to compare the guess to.
+	post-conditions: returns a row that shows whether or not the guess was correct and if 
+	it is incorrect whether the true value is higher or lower. 
+	'''
+	row = {}
+	for key in ["Name"]:
+		row[key] = f"{guess[key]}"
+	for key in [ "Class", "Position", "State/Prov",]:
+			if guess[key] == target[key]:
+				row[key] = f"{guess[key]} 🟩"
+			else:
+				row[key] = f"{guess[key]} ⬜"
+	for key in ["Height"]:
+		if int(removePunctuation(guess[key])) < int(removePunctuation(target[key])):
+			row[key]= f"{guess[key]} ⬆️"
+		elif int(removePunctuation(guess[key])) == int(removePunctuation(target[key])):
+			row[key] = f"{guess[key]} 🟩"
+		else:
+			row[key] = f"{guess[key]} ⬇️"
+	for key in ["#"]:
+		if int(guess[key]) < int(target[key]):
+			row[key] = f"{guess[key]} ⬆️"
+		elif int(guess[key]) == int(target[key]):
+			row[key] = f"{guess[key]} 🟩"
+		else:
+			row[key] = f"{guess[key]} ⬇️"
+	return row
+# main code
+
+def main():	
+	#put title on screen.
+	st.markdown("<h1 style='text-align: center; color: #26A3FF;'>Batter Up!</h1>", unsafe_allow_html=True)
+	st.markdown("<h2 style='text-align: center; color: #FFDE00;'>Whitman Baseball Edition</h2>", unsafe_allow_html=True)
+	
+	#Create and shuffle list of player names
+	if "remaining" not in st.session_state:
+		st.session_state.remaining = list(PlayerInfo.playerDict.keys())
+		random.shuffle(st.session_state.remaining)
+	
+	#when all players are used up, create and shuffle again
+	if len(st.session_state.remaining) == 0:
+		st.session_state.remaining = list(PlayerInfo.playerDict.keys())
+		random.shuffle(st.session_state.remaining)
+			
+	#Get random player
+	if "secretName" not in st.session_state:
+  	  st.session_state.secretName = st.session_state.remaining.pop()
+	secretName =  st.session_state.secretName
+	target = PlayerInfo.playerDict[secretName]
+	
+	#Find videos in dictionary
+	targetVideo = PlayerInfo.playerDict[secretName]["silVideo"]
+	winVideo = PlayerInfo.playerDict[secretName]["Video"]
+	
+	#Create path for videos in folder Swings
+	targetVideo_path = f"Swings/{targetVideo}" 
+	winVideo_path = f"Swings/{winVideo}"
+	
+	#Initialize game state
+	if "game_over" not in st.session_state:
+		st.session_state.game_over = False
+	if "guessInput" not in st.session_state:
+		st.session_state.guessInput = "" 
+	if "tries" not in st.session_state:
+		st.session_state.tries = 0
+	if "guessHistory" not in st.session_state:
+		st.session_state.guessHistory = []
+
+	#get user input
+	inputText = st.empty()
+	guessName = inputText.selectbox("Guess the swing: ", 
+	PlayerInfo.playerDict, index = None, placeholder = "Type or select a player..."
+	, key = "guessInput")
+	
+	#If user loses
+	if st.session_state.tries >= 5:
+		st.session_state.game_over = True
+		st.write("You Lose! The Hitter was " + secretName)
+		st.video(winVideo_path, autoplay = True, muted = True, loop = True)
+		inputText.empty()
+		
+	# show silhouette video
+	if guessName != secretName and st.session_state.tries <= 4:
+		st.video(targetVideo_path, autoplay = True, muted = True, loop = True)	
+	
+	#If guess is correct
+	if guessName == secretName:
+		st.session_state.tries += 1
+		st.write("You Win! The hitter is " + str(secretName))
+		st.video(winVideo_path, autoplay = True, muted = True, loop = True)
+		st.session_state.game_over = True
+		st.write("You got it in " + str(st.session_state.tries) + " tries")
+		inputText.empty()
+	
+	#If game is over prompt for play again
+	if st.session_state.game_over:
+		st.button("Play Again", on_click = resetGame)
+	
+	#If guess is incorrect
+	if guessName != "" and guessName not in PlayerInfo.playerDict:
+		st.write("Player not found!")
+	elif guessName in PlayerInfo.playerDict and guessName != secretName:
+		st.write(str(guessName) + " is not the right hitter")
+		st.session_state.tries += 1
+	
+	#On guess, create table with guess history
+	if guessName:
+		if guessName in PlayerInfo.playerDict:
+			st.subheader("Your Guesses")
+			st.session_state.guessHistory.append(makeRows(PlayerInfo.playerDict[guessName], target))
+			numbers = pd.DataFrame(st.session_state.guessHistory)
+			numbers.index = range(1, len(numbers.index) + 1)
+			st.dataframe(numbers)
+
+
+main()	
+
+
+
---
+++ PlayerInfo.py
@@ -0,0 +1,75 @@
+## Player information dictionary
+playerDict = {
+	"Jonah Chang": {"Name": "Jonah Chang", "Class": "SO", "Position": "INF", "Height": "5'10", "State/Prov":
+	 "BC", "#": "25", "silVideo": "JonahFinalSwing.mp4", "Video": "JonahSwing.mp4"
+	 }, 
+	 "Wyatt Miyamoto": {"Name": "Wyatt Miyamoto", "Class": "SO", "Position": "C", "Height": "5'11", "State/Prov":
+	 "CA", "#": "15", "silVideo": "WyattFinalSwing.mp4", "Video": "WyattSwing.mp4"
+	 },
+	  "Sean Nguyen-Fell": {"Name": "Sean Nguyen-Fell", "Class": "FY", "Position": "OF", "Height": "5'08",
+	  "State/Prov": "CA", "#": "46", "silVideo": "SeanFinalSwing.mp4", "Video": "SeanSwing.mp4"
+	 },
+	  "Tristan \"TJ\" Buehring": {"Name": "Tristan Buehring","Class": "SO", "Position": "OF", "Height": "5'10",
+	  "State/Prov": "WA", "#": "1", "silVideo": "TJFinalSwing.mp4", "Video": "TJSwing.mp4"
+	 },
+	  "Nick Teng": {"Name": "Nick Teng", "Class": "SR", "Position": "INF", "Height": "5'11",
+	  "State/Prov": "CA", "#": "12", "silVideo": "NickFinalSwing.mp4", "Video": "NickSwing.mp4"
+	 },
+	 "Halen Otte": {"Name": "Halen Otte", "Class": "SR", "Position": "C", "Height": "6'00",
+	  "State/Prov": "WA", "#": "4", "silVideo": "HalenFinalSwing.mp4", "Video": "HalenSwing.mp4"
+	 },
+	  "Solomon Kikukawa-Teriong": {"Name": "Solomon Kikukawa-Teriong", "Class": "JR", "Position": "OF", "Height": "5'11",
+	  "State/Prov": "HI", "#": "19", "silVideo": "SolFinalSwing.mp4", "Video": "SolSwing.mp4"
+	 },
+	  "Sam Mieszkowski-Lapping": {"Name": "Sam Mieszkowski-Lapping", "Class": "JR", "Position": "INF", "Height": "6'02",
+	  "State/Prov": "CA", "#": "58", "silVideo": "SamFinalSwing.mp4", "Video": "SamSwing.mp4"
+	 },
+	 "Nate Petz": {"Name": "Nate Petz", "Class": "SO", "Position": "C", "Height": "5'09", "State/Prov":
+	 "CA", "#": "16", "silVideo": "PetzFinalSwing.mp4", "Video": "PetzSwing.mp4"
+	 },
+     "Braydon Olson": {"Name": "Braydon Olson", "Class": "JR", "Position": "INF", "Height": "5'11",
+	  "State/Prov": "WA", "#": "8", "silVideo": "BradFinalSwing.mp4", "Video": "BradSwing.mp4"
+	 },
+	 "Noah Braunel": {"Name": "Noah Braunel", "Class": "JR", "Position": "OF", "Height": "6'01",
+	  "State/Prov": "WA", "#": "10", "silVideo": "NoahFinalSwing.mp4", "Video": "NoahSwing.mp4"
+	 },
+	 "Luke Malzewski": {"Name": "Luke Malzewski", "Class": "JR", "Position": "UTL", "Height": "5'09",
+	  "State/Prov": "WA", "#": "13", "silVideo": "LukeFinalSwing.mp4", "Video": "LukeSwing.mp4"
+	 },
+	 "Christian Rivera": {"Name": "Christian Rivera", "Class": "SR", "Position": "OF", "Height": "5'08",
+	  "State/Prov": "CA", "#": "17", "silVideo": "CRivFinalSwing.mp4", "Video": "CRivSwing.mp4"
+	 },
+     "Carson Davis": {"Name": "Carson Davis", "Class": "FY", "Position": "INF", "Height": "6'03",
+	  "State/Prov": "CA", "#": "29", "silVideo": "CarsonFinalSwing.mp4", "Video": "CarsonSwing.mp4"
+	 },
+	 "Jordan Montemayor": {"Name": "Jordan Montemayor", "Class": "FY", "Position": "INF", "Height": "5'11",
+	  "State/Prov": "WA", "#": "44", "silVideo": "JordanFinalSwing.mp4", "Video": "JordanSwing.mp4"
+	 },
+	 "Landon Runyan": {"Name": "Landon Runyan", "Class": "JR", "Position": "UTL", "Height": "5'11",
+	  "State/Prov": "CA", "#": "48", "silVideo": "LandonFinalSwing.mp4", "Video": "LandonSwing.mp4"
+	 },
+	 "Danny DiMarco": {"Name": "Danny DiMarco", "Class": "FY", "Position": "OF", "Height": "6'00",
+	  "State/Prov": "WA", "#": "49", "silVideo": "DannyFinalSwing.mp4", "Video": "DannySwing.mp4"
+	 },
+     "Tanner Coddington": {"Name": "Tanner Coddington", "Class": "JR", "Position": "P", "Height": "6'04", 
+	  "State/Prov": "CO", "#": "27", "silVideo": "TCoFinalSwing.mp4", "Video": "TCoSwing.mp4"
+	 },
+     "Charlie Coffman": {"Name": "Charlie Coffman", "Class": "JR", "Position": "P", "Height": "5'11",
+	  "State/Prov": "WA", "#": "14", "silVideo": "ChuckFinalSwing.mp4", "Video": "ChuckSwing.mp4"
+	 },
+     "Jordan Hara": {"Name": "Jordan Hara", "Class": "GRAD", "Position": "UTL", "Height": "5'08",
+	  "State/Prov": "HI", "#": "0", "silVideo": "HaraFinalSwing.mp4", "Video": "HaraSwing.mp4"
+	 },
+}
+'''
+	 },
+	 "Carl Moland-Kovash": {"Name": "Carl Moland-Kovash", "Class": "JR", "Position": "P", "Height": "6'06",
+	  "State/Prov": "IL", "#": "33", "silVideo": "CarlFinalSwing.mp4", "Video": "CarlSwing.mp4"
+	 },
+	 "Nathan Wilson": {"Class": "SR", "Position": "INF", "Height": "5'11",
+	  "State/Prov": "TX", "#": "21", "silVideo": "NateWillSwing.mov", "Video": "NateWillVid.mov"
+	 },
+	 "Christopher Nobrega": {"Class": "FY", "Position": "OF", "Height": "5'08",
+	  "State/Prov": "ON", "#": "43", "silVideo": "NobregaSwing.mov", "Video": "NobregaVid.mov"
+	 },
+'''
